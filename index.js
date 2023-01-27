18dbbd78b779de6b2d2a2844a197e7d896b3524a
pallavee <pallaveeratnakar1714@gmail.com>
2023-01-27 11:17:43 +0530

    Deposit_suceess

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const fs = require("fs");
 
-
-
-
 if (process.argv[2] == "CREATE") {
-
-
   let data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
   // console.log(data.User_Details)
   let users = data.User_Details;
 
-  for(let i=0;i<users.length;i++)
-  {
-    if(users[i]["AccountNumber"]==process.argv[3])
-    {
-        return console.log("UserAccount already Exist!")
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]["AccountNumber"] == process.argv[3]) {
+      return console.log("UserAccount already Exist!");
     }
   }
   let payload = {
@@ -27,4 +20,27 @@ if (process.argv[2] == "CREATE") {
   data.User_Details = newData;
   let finalData = JSON.stringify(data);
   fs.writeFileSync("./db.json", finalData, "utf-8");
+
+  
+} else if (process.argv[2] == "DEPOSIT") {
+
+
+  if (process.argv[4] < 0) {
+    return console.log("Not A valid Amount To be Deposited!");
+  }
+  const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+
+  let users = data.User_Details;
+
+  let newData = users.map((el) => {
+    if (el["AccountNumber"] == process.argv[3]) {
+      return { ...el, Balance: el.Balance + Number(process.argv[4]) };
+    } else {
+      return el;
+    }
+  });
+
+  data.User_Details = newData;
+  const final = JSON.stringify(data);
+  fs.writeFileSync("./db.json", final, "utf-8");
 }
