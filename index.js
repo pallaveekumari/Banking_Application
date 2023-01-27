114ddc117ef3969c4900bd1f67e937b5b2543965
pallavee <pallaveeratnakar1714@gmail.com>
2023-01-27 10:54:08 +0530

    CreateAccount_suceess

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+const fs = require("fs");
 
-const fs=require("fs")
 
 
+
+if (process.argv[2] == "CREATE") {
+
+
+  let data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+  // console.log(data.User_Details)
+  let users = data.User_Details;
+
+  for(let i=0;i<users.length;i++)
+  {
+    if(users[i]["AccountNumber"]==process.argv[3])
+    {
+        return console.log("UserAccount already Exist!")
+    }
+  }
+  let payload = {
+    AccountNumber: process.argv[3],
+    Balance: 0,
+    Name: process.argv[4],
+  };
+
+  let newData = [...users, payload];
+  data.User_Details = newData;
+  let finalData = JSON.stringify(data);
+  fs.writeFileSync("./db.json", finalData, "utf-8");
+}
