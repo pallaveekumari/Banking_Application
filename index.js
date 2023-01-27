742912ca4491ee6e2dad0fcbcfca561969c8e626
pallavee <pallaveeratnakar1714@gmail.com>
2023-01-27 12:45:04 +0530

    withdrawal_done

--- index.js
+++ index.js
@@ -20,14 +20,11 @@ if (process.argv[2] == "CREATE") {
   data.User_Details = newData;
   let finalData = JSON.stringify(data);
   fs.writeFileSync("./db.json", finalData, "utf-8");
-
-  
 } else if (process.argv[2] == "DEPOSIT") {
-
-
   if (process.argv[4] < 0) {
     return console.log("Not A valid Amount To be Deposited!");
   }
+
   const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
 
   let users = data.User_Details;
@@ -43,4 +40,50 @@ if (process.argv[2] == "CREATE") {
   data.User_Details = newData;
   const final = JSON.stringify(data);
   fs.writeFileSync("./db.json", final, "utf-8");
+
+} else if (process.argv[2] == "WITHDRAW") {
+  if (process.argv[4] < 0) {
+    return console.log("The Balance for withdrawal is Incorrect!");
+  }
+
+  if (process.argv[4] == 0) {
+    return console.log("Insufficient Balance !");
+  }
+
+  const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+  let users = data.User_Details;
+
+  let newData = users.map((el) => {
+    
+    if (
+      el["AccountNumber"] == process.argv[3] &&
+      process.argv[4] <= el.Balance
+    ) {
+      return { ...el, Balance: el.Balance - Number(process.argv[4]) };
+    } else {
+      return el;
+    }
+  });
+
+  data.User_Details = newData;
+  const final = JSON.stringify(data);
+  fs.writeFileSync("./db.json", final, "utf-8");
+}
+
+else if(process.argv[2]=="BALANCE")
+{
+    const data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+    let users = data.User_Details;
+    let newData=users.map((el)=>{
+        if(el.AccountNumber==process.argv[3])
+        {
+            return console.log(el.Name,el.Balance)
+        }
+        else{
+            return el
+        }
+    })
+   
+    const final = JSON.stringify(data);
+    fs.writeFileSync("./db.json", final, "utf-8");
 }
